6f19518a80b983a19935b9c83723d285b03f6f6c
Christopher Eagle <chris@weblaunchr.com>
2022-04-10 20:40:31 +0100

    added nextjs ts

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+}
+
+export default MyApp
